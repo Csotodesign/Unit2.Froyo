b863b51bcef91312fbfe7fe32ee2c55611cafa5c
Csotodesign <146400157+Csotodesign@users.noreply.github.com>
2023-11-26 10:51:26 -0600

    corrected function to correctly output flavors and flavor count

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 // 1. The user is prompted for froyo flavors upon entering the website "vanilla, strawberry, coffee, etc.."
-const customerOrder = prompt("Please enter the flavors you want, separated by commas.")
+const customerOrder = prompt("Enter flavors separated by commas:");
 
 // 2. The user's input string is split into an array of strings
-const stringArray = customerOrder.split(",");
+const flavors = customerOrder.split(',');
 
 //3. A loop is used to iterate through the array of flavors.
-const order = {}
+const flavorCounts = {};
 
-function countFlavors(order) {
-  for (let i = 0; i < stringArray.length; i++) {
-    const index = stringArray[i]
-    if (order[customerOrder[i]] === undefined) {
-      order[customerOrder[i]] = 1
-    } else {
-      order[customerOrder[i]]
-    }
-  } return order;
+for (const flavor of flavors) {
+  const trimmedFlavor = flavor.trim(); // Removes leading/trailing spaces
+  flavorCounts[trimmedFlavor] = (flavorCounts[trimmedFlavor] || 0) + 1;
 }
 
 //4. An object is used to keep count of how many orders there are of each flavor.
-
-//5. The program correctly counts the number of each flavor in the user's input.
+for (const flavor in flavorCounts) {
+  console.log(`${flavor}: ${flavorCounts[flavor]} orders`);
+}
